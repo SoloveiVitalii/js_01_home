84f9a30f0b7f31fbfbb47fba87a47725360942b2
Vitalii <almamatery500@gmail.com>
2018-09-03 11:25:45 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+
+function solveQuadr(a, b, c) {
+	if (a=2) {
+    return 'X1 = ' + x1 + ' X2 = ' + x2;
+}
+   else if (b= -8) {
+   	return 'X1 = ' + x1 + ' X2 = ' + x2;
+}
+   else if (c=6) {
+   	return 'X1 = ' + x1 + ' X2 = ' + x2;
+}
+
+}
+const result solveQuadr(a,b,c);
+alert(result);
